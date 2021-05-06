7479b0d22ccb1f6cbbdf97dfa989b177df020883
holtzy <yan.holtz@datadoghq.com>
2021-05-06 22:22:21 +0200

    first arc diagram with v4-v6 option

--- js/myParser.js
+++ js/myParser.js
@@ -3,8 +3,6 @@ function myHtmlParser(divFrom, divTo){
   var content = document.getElementById(divFrom).innerText;
   // remove the xmp tag
   content = content.replace(/<xmp>/,"").replace(/<.xmp>/,"").replace(/<\/?span[^>]*>/g,"");
-  //console.log("This is the content of the html fragment!; ")
-  //console.log(content)
   // write the content in the destination div (second argument)
   document.getElementById(divTo).innerHTML = content;
 }
@@ -14,26 +12,22 @@ function myHtmlParser(divFrom, divTo){
 var gvar = this;
 
 
-
-function myJSParser(divFrom, divTo){
+function myJSParser(divFrom){
   // get the content of the origin div (first argument)
   var command = document.getElementById(divFrom).innerText;
   // remove the xmp tag and the script tags
   command = command.replace(/<xmp>/,"").replace(/<.xmp>/,"").replace(/<script>/,"").replace(/<.script>/,"").replace(/<\/?span[^>]*>/g,"");
-  //console.log("This is the content of the JS fragment!; ")
-  //console.log(command)
   // run the JS code
   gvar.eval(command);
 }
 
 // Same as the previous one, but with no HTML before.
+// Use in tutorial pages
 function myJSParserShort(divFrom){
   // get the content of the origin div (first argument)
   var command = document.getElementById(divFrom).innerText;
   // remove the xmp tag and the script tags
   command = command.replace(/<xmp>/,"").replace(/<.xmp>/,"").replace(/<script>/,"").replace(/<.script>/,"").replace(/<\/?span[^>]*>/g,"");
-  //console.log("This is the content of the JS fragment!; ")
-  //console.log(command)
   // run the JS code
   gvar.eval(command);
 }
@@ -45,7 +39,6 @@ function myCodeDownload(filename, idHTML, idJS) {
   var myHtml = document.getElementById(idHTML).innerText;
   var myJs = document.getElementById(idJS).innerText;
   var myText = myHtml + "\n" + myJs
-  console.log(myText)
 
   // create a hidden element and click on it:
   var element = document.createElement('a');
@@ -59,33 +52,3 @@ function myCodeDownload(filename, idHTML, idJS) {
 
   document.body.removeChild(element);
 }
-
-
-
-
-
-
-
-
-
-
-// function myPngExporter(output, mydiv, mytime){
-
-//   // We need for the javascript to be executed before exporting to png
-//   var millisecondsToWait = mytime;
-//   setTimeout(function() {
-
-//     // export the div called mydiv
-//     html2canvas($(mydiv),
-//       {
-//         onrendered: function (canvas) {
-//           var a = document.createElement('a');
-//           // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
-//           a.href = canvas.toDataURL("image/png") //.replace("image/png", "image/octet-stream");
-//           a.download = output;
-//           a.click();
-//         }
-//       });
-
-//   }, millisecondsToWait);
-// }
