88439bac74d74be115346ac040e93a138ff2407b
holtzy <yan.holtz@datadoghq.com>
2021-05-07 11:43:14 +0200

    move all the v4-v6 parsing in myParser.js

--- js/myParser.js
+++ js/myParser.js
@@ -52,3 +52,65 @@ function myCodeDownload(filename, idHTML, idJS) {
 
   document.body.removeChild(element);
 }
+
+
+// UPDATE MAY 2021 - ADD V4 - V6 CONTROL
+function showCodeVersion(version) {
+
+  if(version === "v4"){
+    // Hide all v6 related code chunks
+    allV6 = document.querySelectorAll(".d3v6-chunk")
+    allV6.forEach( el => el.classList.add('d-none'))
+    // Show all v4
+    allV4 = document.querySelectorAll(".d3v4-chunk")
+    allV4.forEach( el => el.classList.remove('d-none'))
+    // Swap the selected button
+    document.getElementById('button-show-v4').classList.add("active")
+    document.getElementById('button-show-v6').classList.remove("active")
+    // Load d3V4
+    $.getScript("https://d3js.org/d3.v4.js", function(){
+      // Run the according code to update the graph
+      myHtmlParser('code-html-v4', 'result')
+      myJSParser('code-js-v4')
+    });
+    // Add event listener to the editable code of v4
+    document.getElementById('code-js-v4').addEventListener("input", function() {
+      myHtmlParser('code-html-v4', 'result')
+      myJSParser('code-js-v4')
+    })
+    // Remove event listener to the editable code of v6
+    document.getElementById('code-js-v6').removeEventListener("input", function() {
+      myHtmlParser('code-html-v6', 'result')
+      myJSParser('code-js-v6')
+    })
+  }
+
+  if(version === "v6"){
+    // Hide all v4 related code chunks
+    allV4 = document.querySelectorAll(".d3v4-chunk")
+    allV4.forEach( el => el.classList.add('d-none'))
+    // Show all v6
+    allV6 = document.querySelectorAll(".d3v6-chunk")
+    allV6.forEach( el => el.classList.remove('d-none'))
+    // Swap the selected button
+    document.getElementById('button-show-v6').classList.add("active")
+    document.getElementById('button-show-v4').classList.remove("active")
+    // Load d3V6
+    $.getScript("https://d3js.org/d3.v6.js", function(){
+      // Run the according code to update the graph
+      myHtmlParser('code-html-v6', 'result')
+      myJSParser('code-js-v6')
+    });
+    // Add event listener to the editable code of v6
+    document.getElementById('code-js-v6').addEventListener("input", function() {
+      myHtmlParser('code-html-v6', 'result')
+      myJSParser('code-js-v6')
+    })
+    // Remove event listener to the editable code of v6
+    document.getElementById('code-js-v4').removeEventListener("input", function() {
+      myHtmlParser('code-html-v4', 'result')
+      myJSParser('code-js-v4')
+    })
+  }
+
+}
